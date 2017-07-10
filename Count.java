9ba91482767104703e9cd6ed9de34f4ed00e6373
anguprakash <anguprakash0005@gmail.com>
2017-07-10 18:18:18 +0530

    Update and rename Digit.java to Count.java

---
+++ Count.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Count{
+  public static void main(String args[])
+  {
+  int n,i;
+  int count=0;
+  Scanner s=new Scanner(System.in);
+  n=s.nextInt();
+  while(n>0)
+  {
+       n=n/10;
+       count++;
+       }
+       System.out.println(count);
+       }
+       }
