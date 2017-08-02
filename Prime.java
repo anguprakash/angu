d2d8b26b37954a1314a7c0bbcf4a2abb25a22502
anguprakash <anguprakash0005@gmail.com>
2017-08-02 10:03:48 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,23 @@
+public class Prime {
+
+    public static void main(String[] args) {
+
+        int low=2, high=6;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+            if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
