9d354fac22419a3b6a6935ef76e67204d29b275d
anguprakash <anguprakash0005@gmail.com>
2017-08-02 14:56:45 +0530

    Create Coversion.java

---
+++ Coversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Conversion{
+  public static void main(String args[])
+  {   int i;
+    String a="30";
+    i=Integer.parseInt(a);
+    System.out.println(i);
+    }
+    }
